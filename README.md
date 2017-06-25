9fe1b5ffffa71ea42a389447a499785ff525fbaa
CoderDream <coderdream@gmail.com>
2017-06-25 21:38:06 +0800

    2-1 第一个Quartz程序

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# quartz-demo
-Java Quartz demo
+# Java定时任务调度工具详解中的Quartz篇
+
+简介：本课程是系列课程Java定时任务调度工具详解中的Quartz篇，本系列课程旨在通过详细讲述Java定时调度工具的基本概念、工具，和这些工具里面包含的各个组件之间的关系，以及如何使用这些工具来实现定时调度功能，让学生能够对Java定时调度工具有一个清晰而准确的认识。然后结合一些经典的使用场景通过手把手的命令行操作进行教学，使同学们得心用手地使用这些定时调度工具来实现自己想要的功能。
+
+ 第1章 课程介绍 
+ 1-1 初识Quartz (12:01)
+
+
+# 第2章 Quartz详解 # 
+
+## 2-1 第一个Quartz程序 (18:33) ##
+
+
+com.coderdream.quartz.HelloScheduler
+
+
+    Current excut is:2017-06-25 21:08:33
+	Current excut is:2017-06-25 21:08:33
+	Hello World!
+	Current excut is:2017-06-25 21:08:35
+	Hello World!`
+
+
+
+
+
+
+
---
+++ src/main/java/com/coderdream/quartz/HelloScheduler.java
@@ -0,0 +1,39 @@
+package com.coderdream.quartz;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class HelloScheduler {
+
+	public static void main(String[] args) throws SchedulerException {
+		// 创建一个JobDetail实例，将该实例与HelloJob Class绑定
+		JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
+				.withIdentity("myJob", "group1").build();
+
+		// 创建一个Trigger实例，定义该Job立即执行，并且每个两秒钟重复一次，直到永远
+		Trigger trigger = TriggerBuilder.newTrigger()
+				.withIdentity("myTrigger", "group1").startNow()
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule()
+						.withIntervalInSeconds(2).repeatForever())
+				.build();
+		// 创建Scheduler实例
+		SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+		Scheduler scheduler = schedulerFactory.getScheduler();
+		scheduler.start();
+		// 打印当前的执行时间，格式为2017-01-01 00:00:00
+		Date date = new Date();
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("Current excut is:" + sf.format(date));
+		scheduler.scheduleJob(jobDetail, trigger);
+	}
+}
